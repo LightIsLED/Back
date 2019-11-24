115a31ef5ec880e91b51f7411be29841128a43fb
Yewon-july <kellychung730@gmail.com>
2019-11-24 19:42:25 +0900

    config

--- init.js
+++ init.js
@@ -3,6 +3,6 @@ var app = require("./app");
 const PORT = process.env.PORT || 2000;
 
 const handleListening = () => 
-    console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: ${PORT}`);
 
 app.listen(PORT,handleListening);
