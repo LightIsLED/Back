bb9c886094f7b061bcaaa78e8b7c73fec37f4776
Yewon-july <kellychung730@gmail.com>
2019-11-26 23:06:47 +0900

    알람 구별

--- controllers/askAlarmList.js
+++ controllers/askAlarmList.js
@@ -12,21 +12,25 @@ const alarmList = async(req, res, next) => {
     console.log(req.body);
     console.log(req.body.action.parameters);
 
+    //오늘 알람 리스트를 물었을 경우
+    ///Answer-alarmListToday
     var scheDate = moment().format('YYYY-MM-DD');
 
+    //특정일의 알람 리스트를 물었을 경우
+    ///Answer-alarmListTheDay
     if((req.body.action.parameters).hasOwnProperty('u_today') === false){
         var dateFormat = (moment().format('YYYY')).toString() + req.body.action.parameters.u_themonth.value + req.body.action.parameters.u_theDay.value;
         console.log('dateFormat: ', dateFormat);
         scheDate = moment(dateFormat).format('YYYY-MM-DD');
-
     }
-
+    //쿼리는 공통적
     var query = "SELECT DISTINCT scheName " + 
         "from SCHEDULES WHERE scheDate=DATE(:scheDate) AND userID=:userID";
         await sequelize.query(query, 
             {replacements:  {scheDate: scheDate, userID: req.body.action.parameters.userID_1.value}, type: Sequelize.QueryTypes.SELECT}
         ).then((results) => {
             console.log(results);
+            //울릴 알람이 여러개일 경우
             let resultList = '';
             for(var i = 0; i < results.length; i++){
                 resultList = resultList +results[i].scheName;
@@ -34,12 +38,22 @@ const alarmList = async(req, res, next) => {
                     resultList = resultList + ', '
                 }
             }
+            //RESPONSE SAMPLE 형식에 맞춤
             let resObj = json.resObj();
 
             resObj.version = req.body.version;
-            resObj.output.medicineList_today = resultList;
+            //오늘 알람을 물었을 경우
+            if((req.body.action.parameters).hasOwnProperty('u_today') === true){
+                resObj.output.medicineList_today = resultList;
+            }
+            //특정일 알람을 물었을 경우
+            else{
+                resObj.output.medicineList_theDay = resultList;
+            }
 
             console.log(resObj);
+            
+            //NUGU에 전달
             res.json(resObj);
             res.end();
             return;
@@ -48,24 +62,6 @@ const alarmList = async(req, res, next) => {
             next(error);
         });
 }
-
-/*
-    else if(parameter.u_today === null && (parameter.u_themonth!= null && parameter.u_theDay!=null)){
-        var date = moment([moment().format('YYYY'), parameter.u_themonth, parameter.u_theDay]).format('YYYY-MM-DD');
-        var query = "SELECT DISTINCT scheName AS medicineList_theDay " + 
-        "from SCHEDULES WHERE scheDate=DATE(:scheDate) AND userID=:userID";
-        await sequelize.query(query, 
-            {replacements: {scheDate: date, userID: 1}, type: Sequelize.QueryTypes.SELECT}
-        ).then((result) => {
-            console.log(result);
-            //JSON 형식 수정 필요
-            res.json(result);
-        }).catch((error) => {
-            console.error(error);
-            next(error);
-        });
-    }
-*/
 
 module.exports = {
     alarmList
